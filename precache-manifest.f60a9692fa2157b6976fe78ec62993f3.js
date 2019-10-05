2b579956d54bf19c9acda51a3128c0df4983415b
smd9788 <smd09876@gmail.com>
2019-10-05 17:53:33 -0400

    Updates

---
+++ precache-manifest.f60a9692fa2157b6976fe78ec62993f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82444aa0b6cd5def98e2",
+    "url": "/static/js/main.82444aa0.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "82444aa0b6cd5def98e2",
+    "url": "/static/css/main.43ed142d.chunk.css"
+  },
+  {
+    "revision": "99dab6382b6a153ca27131ac1a602d21",
+    "url": "/index.html"
+  }
+];
